e9426f4f41aa6488ed89d3da0e745782de426504
Ishanu Goswami <138703096+ishanu2744@users.noreply.github.com>
2023-07-06 23:52:03 +0530

    Update QR_Reader.java

--- QR_Code_Generation/QR_Reader.java
+++ QR_Code_Generation/QR_Reader.java
@@ -10,7 +10,7 @@ import java.io.FileOutputStream;
 public class QR_Reader {
     public static void main(String[] args) throws Exception{
         File file=new File("C:\\Users\\Ishanu Goswami\\IdeaProjects\\qr-code.png");
-        String content="file:///C:/Users/Ishanu%20Goswami/Documents/Web%20Development/Home_Website.html";
+        String content="Hello!!! This is Ishanu Goswami...";
         ByteArrayOutputStream out =QRCode.from(content).to(ImageType.PNG).stream();
         FileOutputStream fos=new FileOutputStream(file);
         fos.write(out.toByteArray());
